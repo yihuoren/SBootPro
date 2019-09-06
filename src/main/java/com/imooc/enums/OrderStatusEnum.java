78296b97e70282b290164290f1ba4f19b56c7dc9
yihuoren <18539912827@163.com>
2019-09-06 13:40:33 +0800

    订单状态枚举值更新

--- src/main/java/com/imooc/enums/OrderStatusEnum.java
+++ src/main/java/com/imooc/enums/OrderStatusEnum.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 public enum OrderStatusEnum {
     NEW(0,"新订单"),
     FINISHED(1,"完结"),
-    CANCEL(3,"已取消");
+    CANCEL(2,"已取消");
 
     private Integer code;
     private String message;
