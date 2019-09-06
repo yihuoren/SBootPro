2486b52dcaeb4da340c533cff8861e135447aa55
yihuoren <18539912827@163.com>
2019-09-06 16:10:38 +0800

    新增买家订单API

--- src/main/java/com/imooc/controller/BuyerOrderController.java
+++ src/main/java/com/imooc/controller/BuyerOrderController.java
@@ -1,8 +1,36 @@
 package com.imooc.controller;
 
+import com.imooc.VO.ResultVO;
+import com.imooc.enums.ResultEnum;
+import com.imooc.exception.SellException;
+import com.imooc.form.OrderForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Map;
+
 /**
  * @Author wanghl
  * @Date: Created in 15:28 2019/9/6 0006
  */
+@RestController
+@RequestMapping("/buyer/order")
+@Slf4j
 public class BuyerOrderController {
+    //创建订单
+    public ResultVO<Map<String,String>> create(@Valid OrderForm orderForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            log.error("【创建订单】参数不正确, orderForm={}", orderForm);
+            throw new SellException(ResultEnum.PARAM_ERROR.getCode(),bindingResult.getFieldError().getDefaultMessage());
+        }
+        return null;
+    }
+    //订单列表
+
+    //订单详情(查询单个订单)
+
+    //取消订单
 }
