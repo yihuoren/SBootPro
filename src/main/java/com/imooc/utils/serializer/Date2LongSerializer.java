fe61bc7b711a0344eb3e52c8b3910816d603bffd
yihuoren <18539912827@163.com>
2019-09-07 16:10:33 +0800

    新增系列化工具类，将JavaBean中Date毫秒级转秒级

---
+++ src/main/java/com/imooc/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,22 @@
+package com.imooc.utils.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 将JavaBean中Date毫秒级转秒级
+ * @Author wanghl
+ * @Date: Created in 15:50 2019/9/7 0007
+ */
+public class Date2LongSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeNumber(date.getTime() / 1000);
+    }
+}
