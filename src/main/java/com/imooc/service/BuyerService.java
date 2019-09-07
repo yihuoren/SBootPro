5e5ded30e677c2ee95f5043c22a760e07718f2d5
yihuoren <18539912827@163.com>
2019-09-07 18:14:19 +0800

    新增买家service，用来提取BuyerOrderController中的业务逻辑，让控制层保持清洁，让业务层专注于处理业务逻辑

--- src/main/java/com/imooc/service/BuyerService.java
+++ src/main/java/com/imooc/service/BuyerService.java
@@ -1,8 +1,15 @@
 package com.imooc.service;
 
+import com.imooc.dto.OrderDTO;
+
 /**
+ * 买家service
  * @Author wanghl
  * @Date: Created in 17:29 2019/9/7 0007
  */
 public interface BuyerService {
+    //订单详情（查询一个订单）
+    OrderDTO findOrderOne(String openid,String orderId);
+    //取消订单
+    OrderDTO cancelOrder(String openid,String orderId);
 }
